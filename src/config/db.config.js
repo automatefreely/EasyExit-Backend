6322647b0894e0a36898183f791e7466768230b6
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2024-07-04 12:31:16 +0530

    Notification creds

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,11 +1,17 @@
-import ***REMOVED*** PrismaClient ***REMOVED*** from '@prisma/client';
-const prisma = new PrismaClient();
+import ***REMOVED*** Prisma, PrismaClient ***REMOVED*** from '@prisma/client';
+const prisma = new PrismaClient(***REMOVED***
+***REMOVED******REMOVED***errorFormat: 'pretty',
+***REMOVED***);
 export default prisma;
 
 export async function connDB() ***REMOVED***
 ***REMOVED******REMOVED***try ***REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***await prisma.$connect();
 ***REMOVED******REMOVED******REMOVED******REMOVED***console.log(`🎉 Connected to Database`);
+***REMOVED******REMOVED******REMOVED******REMOVED***prisma.$on('beforeExit', async () => ***REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***await prisma.$disconnect();
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log(`🚪 Database connection closed`);
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***);
 ***REMOVED******REMOVED******REMOVED*** catch (error) ***REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***console.log(error);
 ***REMOVED******REMOVED******REMOVED***
